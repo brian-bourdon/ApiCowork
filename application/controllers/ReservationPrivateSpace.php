9a51c42adc032dc7d13e50c046c309ecf230f7bc
brian-bourdon <brian78.b@gmail.com>
2020-09-27 22:16:46 +0200

    Reservation

---
+++ application/controllers/ReservationPrivateSpace.php
@@ -0,0 +1,73 @@
+<?php
+//https://cowork-paris.000webhostapp.com/index.php/item
+require APPPATH . 'libraries/REST_Controller.php';
+     
+class ReservationPrivateSpace extends REST_Controller {
+    
+      /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+       
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_get($id = 0)
+    {
+        if(!empty($id)){
+            $data = $this->db->get_where("reservation_espace_privatif", ['id' => $id])->row_array();
+        }else{
+            $data = $this->db->get("reservation_espace_privatif")->result();
+        }
+     
+        $this->response($data, REST_Controller::HTTP_OK);
+    }
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_post()
+    {
+        $input = $this->input->post();
+        $this->db->set($input);
+        $this->db->insert('reservation_espace_privatif', $input);
+     
+        $this->response(['Reservation created successfully.'], REST_Controller::HTTP_OK);
+    } 
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function update_post($id)
+    {
+        
+        $input = $this->input->post();
+        $this->db->update('reservation_espace_privatif', $input, array('id'=>$id));
+     
+        $this->response(['Reservation updated successfully.'], REST_Controller::HTTP_OK);
+    }
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_delete($id)
+    {
+        $this->db->delete('user', array('id'=>$id));
+       
+        $this->response(['Reservation deleted successfully.'], REST_Controller::HTTP_OK);
+    }
+        
+}
